d2f4efd82b181774a78d59317b429298e4dc6e95
OsamuMurata1077 <murata@g.neec.ac.jp>
2026-04-04 14:58:32 +0900

    01_01.KAMATA ENGINE 19枚目

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,22 @@
 #include <Windows.h>
+#include "KamataEngine.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	// エンジンの初期化
+	KamataEngine::Initialize();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
