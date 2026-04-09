4c76bb6f92078cdd739e01022cab8941401fea08
GSManager <GSManager@MSI>
2026-04-09 10:51:26 +0900

    学校PCにクローン

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -8,7 +8,7 @@ using namespace KamataEngine;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
 	// エンジンの初期化
-	KamataEngine::Initialize(L"LE3*_00_カマタ_タロウ_CG4");
+	KamataEngine::Initialize(L"LE3*_00_カマトゥ_タロウ_CG4");
 
 	// DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
